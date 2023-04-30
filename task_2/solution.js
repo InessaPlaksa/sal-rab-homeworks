142d4416aa4774e949def49290034b778e1cd2b6
Inessa Plaksa <byplaksa@bk.ru>
2023-04-30 15:45:55 +0300

    initial commit

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,16 +5,16 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum;// создайте переменную shippingSum
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
+    if(productsSum=0) // если productsSum равно 0,
+    else shippingSum = 0; // то shippingSum присвоить значение 0
 
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
+    if(productsSum>=freeShippingMinSum) // если productsSum Больше или равна freeShippingMinSum,
+    else shippingSum = 0; // то shippingSum присвоить значение 0
 
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
+    if(productsSum>0 $$ <freeShippingMinSum) // если productsSum больше 0 и меньше freeShippingMinSum,
+    else shippingSum = shippingPrice; // то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
 
