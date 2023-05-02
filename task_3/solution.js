3d9a94dbc280311db23bbfbed47f84d2b3b78f92
Inessa Plaksa <byplaksa@bk.ru>
2023-05-02 21:20:47 +0300

    10

--- task_3/solution.js
+++ task_3/solution.js
@@ -15,11 +15,11 @@ function renderCartItem(item) {
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
     let itemCountText = "";// 1. Создайте переменную itemCountText, равную пустой строке
-    itemCountText = itemCountText + item.count; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    itemCountText = itemCountText + item(0).count; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     itemCountText = itemCountText + "x";// 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    itemCountText = itemCountText + item.price;// 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
+    itemCountText = itemCountText + item(0).price;// 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     itemCountText = itemCountText + "₽ =";// 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    let sum = item.count * item.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+    let sum = item(0).count * item(0).price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     itemCountText = itemCountText + sum; // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText = itemCountText + " ₽ ";// 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
